fe7adac6f424b7f54f7457b1e541111e32bf7c96
mebs933 <126038921+mebs933@users.noreply.github.com>
2023-12-15 04:49:56 +0000

    Fontsize slider attempt 2

--- public/fontsizeslider.js
+++ public/fontsizeslider.js
@@ -1,7 +1,6 @@
 document.getElementById("fontSlider").oninput = function() {
-    var value = (this.value-this.min)/(this.max-this.min)*100
-    this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + value + '%, #fff ' + value + '%, white 100%)'
-    var fontSize = this.value;
-    document.getElementById("text").style.fontSize = fontSize + "px";
-  };
-  
+  var value = (this.value-this.min)/(this.max-this.min)*100
+  this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + value + '%, #fff ' + value + '%, white 100%)'
+  var fontSize = this.value;
+  document.getElementById("text").style.fontSize = fontSize + "px";
+};
