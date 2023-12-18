25f9518ca441b4f679718a93cb8dd721320efc72
mebs933 <126038921+mebs933@users.noreply.github.com>
2023-12-18 02:59:31 +0000

    Debug

--- public/fontsizeslider.js
+++ public/fontsizeslider.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   var slider = document.getElementById("fontSlider");
   slider.oninput = function() {
-    var fontSize = this.value;
+    var fontSize = Number(this.value); // Parse the value as a number
     document.getElementById("text").style.fontSize = fontSize + "px";
   };
 });
