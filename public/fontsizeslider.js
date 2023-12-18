18f4a870325a1bc8a677c73daeb5af323c8b0ee1
mebs933 <126038921+mebs933@users.noreply.github.com>
2023-12-18 02:11:27 +0000

    Fontsizeslider bug fix

--- public/fontsizeslider.js
+++ public/fontsizeslider.js
@@ -1,6 +1,8 @@
 document.getElementById("fontSlider").oninput = function() {
-  var value = (this.value-this.min)/(this.max-this.min)*100
-  this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + value + '%, #fff ' + value + '%, white 100%)'
+  var fontSize = this.value;
+  document.getElementById("text").style.fontSize = fontSize + "px";
+};
+document.getElementById("fontSlider").oninput = function() {
   var fontSize = this.value;
   document.getElementById("text").style.fontSize = fontSize + "px";
 };
