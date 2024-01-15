e7a25b2da6b8d871d72b95b5fb4331e6c12484d5
mebs933 <126038921+mebs933@users.noreply.github.com>
2024-01-15 03:28:19 +0000

    clientjs update

--- public/client.js
+++ public/client.js
@@ -1,11 +1,8 @@
-import io from 'socket.io-client';
-import transcriptStorage from './transcriptstorage.js'; // Assuming you have this module to handle storage
+import socketIO from 'socket.io-client';
+import transcriptStorage from './transcriptstorage.js';
 
 async function getMicrophone() {
-  const userMedia = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-  });
-
+  const userMedia = await navigator.mediaDevices.getUserMedia({ audio: true });
   return new MediaRecorder(userMedia);
 }
 
@@ -13,17 +10,14 @@ async function openMicrophone(microphone, socket) {
   await microphone.start(500);
 
   microphone.onstart = () => {
-    console.log("client: microphone opened");
     document.body.classList.add("recording");
   };
 
   microphone.onstop = () => {
-    console.log("client: microphone closed");
     document.body.classList.remove("recording");
   };
 
   microphone.ondataavailable = (e) => {
-    console.log("client: sent data to websocket");
     socket.emit("packet-sent", e.data);
   };
 }
@@ -35,8 +29,6 @@ async function closeMicrophone(microphone) {
 async function start(socket) {
   const listenButton = document.getElementById("record");
   let microphone;
-
-  console.log("client: waiting to open microphone");
 
   listenButton.addEventListener("click", async () => {
     if (!microphone) {
@@ -51,16 +43,15 @@ async function start(socket) {
 
 window.addEventListener("load", () => {
   const options = { transports: ["websocket"] };
-  const socket = io(options);
+  const socket = socketIO(options);
 
   socket.on("connect", async () => {
-    console.log("client: connected to websocket");
     await start(socket);
   });
 
   socket.on("transcript", (transcript) => {
     if (transcript !== "") {
-      // Here we update the transcript storage instead of directly manipulating the UI
+      // Update the transcript storage instead of directly manipulating the UI
       transcriptStorage.addTranscript(transcript);
     }
   });
