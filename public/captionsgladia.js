fe12ff213bc05edd99eb6e5b75fd38d5499c807c
mebs933 <126038921+mebs933@users.noreply.github.com>
2024-03-05 02:53:33 +0000

    Adjust debug in module naming in index

--- public/captionsgladia.js
+++ public/captionsgladia.js
@@ -1,11 +1,22 @@
 //captionsgladia.js
-import TranscriptStorageGladia from './transcriptstoragegladia.js';
+import transcriptStorage from './transcriptstoragegladia.js';
 
 const captionsElement = document.getElementById("captionsgladia");
 const textElement = document.getElementById("textgladia");
-const transcriptStorage = new TranscriptStorageGladia();
 
-transcriptStorage.onNewTranscript = (newTranscript) => {
+// Flag to indicate if transcription has started
+let transcriptionStartedGladia = false;
+
+transcriptStorage.onNewTranscript((newTranscript) => {
+  // If transcription has not started, clear the placeholder text
+  if (!transcriptionStartedGladia) {
+    textElement.innerHTML = "";
+    transcriptionStartedGladia = true;
+  }
+
+  // Append the new transcript to the previous text
   textElement.innerHTML += `<span>${newTranscript}</span>`;
+
+  // Scroll to the bottom of the captions element
   captionsElement.scrollTop = captionsElement.scrollHeight;
-};
+});
